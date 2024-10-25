73d1d9b696e6fcf21f3a7d336b7c51c16831e6cb
Himel-032 <139494430+Himel-032@users.noreply.github.com>
2024-10-25 23:36:16 +0600

    edited and added

--- Main.cpp
+++ Main.cpp
@@ -1,36 +1,40 @@
-#include<bits/stdc++.h>
-#include"SolveLinearBy_Himel.h"
-#include"RK4_Himel.h"
+#include <bits/stdc++.h>
+#include "SolveLinearBy_Himel.h"
+#include "RK4_Himel.h"
+#include "non_linear.h"
+#include "MatrixInversion.h"
 
 using namespace std;
 int main()
 {
-   
-    cout<<"1.Solve Linear equations: "<<endl;
-    cout<<"2.Solve Non-Linear equations: "<<endl;
-    cout<<"3.Solve Differential Equation(Runge - Kutta Method): "<<endl;
-    cout<<"4.Matrix Inversion: "<<endl<<endl; 
-    cout<<"Enter the operation you want to solve : ";
-    
-    int cs; cin>>cs;
-    cout<<endl; 
-    switch(cs){
-        case 1: solveLinear();
-        break;
-        /*case 2: 
+    while (1)
+    {
+        cout << "1.Solve Linear equations: " << endl;
+        cout << "2.Solve Non-Linear equations: " << endl;
+        cout << "3.Solve Differential Equation(Runge - Kutta Method): " << endl;
+        cout << "4.Matrix Inversion: " << endl
+             << endl;
+        cout << "Enter the operation you want to solve : ";
+
+        int cs;
+        cin >> cs;
+        cout << endl;
+        switch (cs)
+        {
+        case 1:
+            solveLinear();
             break;
-        */
+        case 2:
+            non_linear();
+            break;
         case 3:
             solveDifferentialEquation();
             break;
-            /*
         case 4:
             MakeInverse();
             break;
-            */
-         
-        default :
-         cout<<"Invalid selection!"<<endl;
+        default:
+            cout << "Invalid selection!" << endl;
+        }
     }
-
 }
