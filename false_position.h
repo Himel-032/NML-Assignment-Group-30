28546985c37e48f33c4802588dd6d823dbd8139d
Himel-032 <139494430+Himel-032@users.noreply.github.com>
2024-10-26 09:12:54 +0600

    modified

--- false_position.h
+++ false_position.h
@@ -1,39 +1,64 @@
-#ifndef FALSE_POSITION_H
-#define FALSE_POSITION_H
+#ifndef FALSE_POSITION_FP_H
+#define FALSE_POSITION_FP_H
 
-#include <vector>
-#include <cmath>
-#include <iostream>
-#include <set>
-#include <iomanip>
-
+#include <bits/stdc++.h>
 using namespace std;
-double evaluate_Polynomial(const vector<double> &coeffs, double x)
+
+const double TOLERANCE_FP = 1e-6;
+const int MAX_ITERATIONS_FP = 100;
+
+int degree_fp;
+vector<double> coeffs_fp;
+double interval_start_fp;
+double interval_end_fp;
+
+void get_inputs_fp()
+{
+    cout << "Enter the highest power of x: ";
+    cin >> degree_fp;
+    coeffs_fp.resize(degree_fp + 1);
+
+    cout << "Enter the coefficients (from highest degree to lowest): ";
+    for (int i = 0; i <= degree_fp; i++)
+    {
+        cin >> coeffs_fp[i];
+    }
+
+    cout << "Enter the interval (start end): ";
+    cin >> interval_start_fp >> interval_end_fp;
+}
+
+double evaluate_fp(double x)
 {
     double result = 0.0;
-    int degree = coeffs.size() - 1;
-    for (int i = 0; i <= degree; ++i)
+    for (int i = 0; i <= degree_fp; i++)
     {
-        result += coeffs[i] * pow(x, degree - i);
+        result += coeffs_fp[i] * pow(x, degree_fp - i);
     }
     return result;
 }
 
-double falsePositionSingleRoot(const vector<double> &coeffs, double a, double b, double tol)
+bool is_root_fp(double x)
 {
-    double c;
+    return abs(evaluate_fp(x)) < TOLERANCE_FP;
+}
 
-    while ((b - a) >= tol)
+double false_position_fp(double a, double b)
+{
+    double c = a;
+    for (int i = 0; i < MAX_ITERATIONS_FP; i++)
     {
+        double fa = evaluate_fp(a);
+        double fb = evaluate_fp(b);
+        c = (a * fb - b * fa) / (fb - fa);
+        double fc = evaluate_fp(c);
 
-        c = a - (evaluate_Polynomial(coeffs, a) * (b - a)) / (evaluate_Polynomial(coeffs, b) - evaluate_Polynomial(coeffs, a));
-
-        if (fabs(evaluate_Polynomial(coeffs, c)) < tol)
+        if (is_root_fp(c))
         {
             return c;
         }
 
-        if (evaluate_Polynomial(coeffs, c) * evaluate_Polynomial(coeffs, a) < 0)
+        if (fa * fc < 0)
         {
             b = c;
         }
@@ -41,96 +66,48 @@ double falsePositionSingleRoot(const vector<double> &coeffs, double a, double b,
         {
             a = c;
         }
+
+        if (abs(b - a) < TOLERANCE_FP)
+        {
+            return c;
+        }
+    }
+    return c;
+}
+
+void find_roots_fp()
+{
+    vector<double> roots_fp;
+    double step_fp = 0.1;
+
+    for (double x = interval_start_fp; x < interval_end_fp; x += step_fp)
+    {
+        double a = x;
+        double b = x + step_fp;
+
+        if (evaluate_fp(a) * evaluate_fp(b) < 0)
+        {
+            double root = false_position_fp(a, b);
+
+            if (roots_fp.empty() || abs(root - roots_fp.back()) >= TOLERANCE_FP)
+            {
+                roots_fp.push_back(root);
+            }
+        }
     }
 
-    return c;
+    cout << "Roots found in the interval [" << interval_start_fp << ", " << interval_end_fp << "]:\n";
+    for (double root : roots_fp)
+    {
+        cout << fixed << setprecision(3) << root << " ";
+    }
+    cout << endl;
 }
 
 void false_position()
 {
-    int degree;
-    cout << "Enter the degree of the polynomial: ";
-    cin >> degree;
-
-    degree++;
-
-    vector<double> coeffs(degree);
-    cout << "Enter the coefficients of the polynomial from highest degree to lowest (constant last):" << endl;
-    for (int i = 0; i < degree; ++i)
-    {
-        cin >> coeffs[i];
-    }
-
-    double tol = 0.0001;
-
-    char choice;
-    cout << "Do you want to set a custom tolerance (y/n)? ";
-    cin >> choice;
-
-    if (choice == 'y' || choice == 'Y')
-    {
-        cout << "Enter the tolerance (error threshold): ";
-        cin >> tol;
-    }
-
-    int numIntervals;
-    cout << "Enter the number of intervals you want to search for roots: ";
-    cin >> numIntervals;
-
-    set<double> roots;
-
-    for (int i = 0; i < numIntervals; ++i)
-    {
-        double a, b;
-        cout << "Enter interval " << i + 1 << " (e.g., -10 10): ";
-        cin >> a >> b;
-
-        double fa = evaluate_Polynomial(coeffs, a);
-        double fb = evaluate_Polynomial(coeffs, b);
-
-        if (fabs(fa) < tol)
-        {
-            roots.insert(a);
-            cout << "Root found at endpoint: " << fixed << setprecision(4) << a << endl;
-        }
-
-        if (fabs(fb) < tol)
-        {
-            roots.insert(b);
-            cout << "Root found at endpoint: " << fixed << setprecision(4) << b << endl;
-        }
-
-        if (fa * fb < 0)
-        {
-            double root = falsePositionSingleRoot(coeffs, a, b, tol);
-            roots.insert(root);
-
-            cout << "Root found in the interval (" << a << ", " << b << "): " << fixed << setprecision(4) << root << endl;
-        }
-        else if (fa == 0 || fb == 0)
-        {
-
-            cout << "Root found at endpoint in the interval (" << a << ", " << b << ")." << endl;
-        }
-        else
-        {
-            cout << "No sign change in the interval (" << a << ", " << b << "). "
-                 << "f(" << a << ") = " << fa << ", f(" << b << ") = " << fb << ". No root found." << endl;
-        }
-    }
-
-    if (roots.empty())
-    {
-        cout << "No roots found in the specified intervals." << endl;
-    }
-    else
-    {
-        cout << "Roots found:" << endl;
-        for (double root : roots)
-        {
-            cout << fixed << setprecision(4) << root << endl;
-        }
-    }
+    get_inputs_fp();
+    find_roots_fp();
 }
 
 #endif
