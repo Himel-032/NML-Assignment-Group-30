6a2a46a37e9debf9847aff96c24fb0856c384d66
Himel-032 <139494430+Himel-032@users.noreply.github.com>
2024-10-26 09:11:40 +0600

    Modified

---
+++ non_linear_shahporan.h
@@ -0,0 +1,43 @@
+#include <bits/stdc++.h>
+#include "bisection.h"
+#include "false_position.h"
+#include "secant.h"
+#include "newton_raphson.h"
+
+using namespace std;
+
+int non_linear()
+{
+    int method;
+    cout << "Select the method to use:\n1. Bisection\n2. False Position\n3. Secant\n4. Newton-Raphson\n";
+    cin >> method;
+
+    switch (method)
+    {
+    case 1:
+    {
+        bisection();
+        break;
+    }
+    case 2:
+    {
+        false_position();
+        break;
+    }
+    case 3:
+    {
+        secant();
+        break;
+    }
+    case 4:
+    {
+        newton_raphson();
+        break;
+    }
+    default:
+        cout << "Invalid choice. Please select a method between 1 and 4." << endl;
+        break;
+    }
+
+    return 0;
+}
