0a4e0dac31476363e6f7fa22a1b27beacb37cf3d
Himel-032 <139494430+Himel-032@users.noreply.github.com>
2024-10-26 09:12:16 +0600

    modified

--- newton_raphson.h
+++ newton_raphson.h
@@ -1,120 +1,101 @@
 #ifndef NEWTON_RAPHSON_H
 #define NEWTON_RAPHSON_H
 
-#include <vector>
-#include <cmath>
-#include <iostream>
-#include <set>
-#include <iomanip>
-
+#include <bits/stdc++.h>
 using namespace std;
 
-double evaluate_polynomial(const vector<double> &coeffs, double x)
+double tolerance = 1e-6;
+int maxIterations = 100;
+int degree;
+vector<double> coeffs;
+double initialGuess;
+
+void getInputs()
+{
+    cout << "Enter the highest power of x: ";
+    cin >> degree;
+    coeffs.resize(degree + 1);
+    cout << "Enter the coefficients (from highest degree to lowest): ";
+    for (int i = 0; i <= degree; i++)
+    {
+        cin >> coeffs[i];
+    }
+    cout << "Enter the initial guess: ";
+    cin >> initialGuess;
+}
+
+double evaluate(double x)
 {
     double result = 0.0;
-    int degree = coeffs.size() - 1;
-    for (int i = 0; i <= degree; ++i)
+    int currentDegree = coeffs.size() - 1;
+    for (double coeff : coeffs)
     {
-        result += coeffs[i] * pow(x, degree - i);
+        result += coeff * pow(x, currentDegree--);
     }
     return result;
 }
 
-double evaluate_derivative(const vector<double> &coeffs, double x)
+double numericalDerivative(double x)
 {
-    double result = 0.0;
-    int degree = coeffs.size() - 1;
-    for (int i = 0; i < degree; ++i)
-    {
-        result += coeffs[i] * (degree - i) * pow(x, degree - i - 1);
-    }
-    return result;
+    double h = 1e-5;
+    return (evaluate(x + h) - evaluate(x)) / h;
 }
 
-double newtonRaphsonMethod(const vector<double> &coeffs, double x0, double tol, int maxIter)
+double newtonRaphsonMethod(double initialGuess)
 {
-    double x1;
-    for (int i = 0; i < maxIter; ++i)
+    double x = initialGuess;
+    for (int i = 0; i < maxIterations; i++)
     {
-        double f0 = evaluate_polynomial(coeffs, x0);
-        double f0_prime = evaluate_derivative(coeffs, x0);
+        double f_x = evaluate(x);
+        double f_prime_x = numericalDerivative(x);
 
-        if (fabs(f0_prime) < tol)
+        if (abs(f_prime_x) < tolerance)
+            return x;
+
+        double x_new = x - f_x / f_prime_x;
+        if (abs(x_new - x) < tolerance)
+            return x_new;
+
+        x = x_new;
+    }
+    return x;
+}
+
+void findRoots()
+{
+    set<double> roots;
+    for (double guess = initialGuess - 1; guess <= initialGuess + 1; guess += 0.1)
+    {
+        double root = newtonRaphsonMethod(guess);
+
+        bool isUnique = true;
+        for (double r : roots)
         {
-            cout << "Derivative too small, stopping iteration." << endl;
-            return x0;
+            if (abs(root - r) < tolerance)
+            {
+                isUnique = false;
+                break;
+            }
         }
 
-        x1 = x0 - f0 / f0_prime;
-
-        if (fabs(x1 - x0) < tol)
+        if (isUnique && abs(evaluate(root)) < tolerance)
         {
-            return x1;
+            roots.insert(root);
         }
-
-        x0 = x1;
     }
 
-    return x1;
+    cout << "Roots found:\n";
+    for (double root : roots)
+    {
+        cout << fixed << setprecision(3) << root << " ";
+    }
+    cout << endl;
 }
 
 void newton_raphson()
 {
-    int degree;
-    cout << "Enter the degree of the polynomial: ";
-    cin >> degree;
-
-    degree++;
-
-    vector<double> coeffs(degree);
-    cout << "Enter the coefficients of the polynomial from highest degree to lowest (constant last):" << endl;
-    for (int i = 0; i < degree; ++i)
-    {
-        cin >> coeffs[i];
-    }
-
-    double tol = 0.0001;
-    int maxIter = 100;
-
-    char choice;
-    cout << "Do you want to set a custom tolerance (y/n)? ";
-    cin >> choice;
-
-    if (choice == 'y' || choice == 'Y')
-    {
-        cout << "Enter the tolerance (error threshold): ";
-        cin >> tol;
-    }
-
-    int numInitialGuesses;
-    cout << "Enter the number of initial guesses you want to search for roots: ";
-    cin >> numInitialGuesses;
-
-    set<double> roots;
-
-    for (int i = 0; i < numInitialGuesses; ++i)
-    {
-        double x0;
-        cout << "Enter initial guess " << i + 1 << ": ";
-        cin >> x0;
-
-        double root = newtonRaphsonMethod(coeffs, x0, tol, maxIter);
-        roots.insert(root);
-        cout << "Root found: " << fixed << setprecision(4) << root << endl;
-    }
-
-    if (roots.empty())
-    {
-        cout << "No roots found in the specified intervals." << endl;
-    }
-    else
-    {
-        cout << "Unique roots found:" << endl;
-        for (double root : roots)
-        {
-            cout << fixed << setprecision(4) << root << endl;
-        }
-    }
+    getInputs();
+    findRoots();
 }
 
 #endif
